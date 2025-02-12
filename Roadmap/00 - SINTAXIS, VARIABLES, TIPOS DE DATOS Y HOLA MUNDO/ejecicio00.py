8965fbc5aea42cc96f03ce1593077764418e55cf
AoS <aos.fontana.dev@gmail.com>
2025-02-12 20:55:00 -0300

    ejercicio00

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/ejecicio00.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/ejecicio00.py
@@ -1,2 +1,30 @@
 # https://www.python.org/
 
+# Ejemplo de comentario.
+
+# Comentario de
+# varias lineas
+
+"""
+    Ejemplo
+    Multilinea
+    con docstrings
+"""
+
+variable = "hola"
+
+#Python no posee constantes, no obstante
+#por convencion se utilizan mayusculas
+CONSTANTE = "mundo"
+
+strings = "hola"
+enteros = 10
+flotantes = 1.0
+complejos = 1j
+booleanos = True | False
+listas = [1, 2, 3]
+tuplas = (1, 2, 3)
+sets = {1, 2, 3}
+diccionarios = {"a": 1, "b": 2, "c": 3}
+
+print("¡Hola, Python!")
